d1f8f0f3fa96c3e57300755405e3c290cd99af7b
liunh1220 <547119066@qq.com>
2019-10-07 20:29:45 +0800

    修改配置

--- src/main/java/com/dream/city/BackendApplication.java
+++ src/main/java/com/dream/city/BackendApplication.java
@@ -1,11 +1,13 @@
 package com.dream.city;
 
+import com.dream.city.base.config.RedisConfig;
+import com.dream.city.base.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@EnableCaching
+@Import({RedisConfig.class, SwaggerConfig.class})
 public class BackendApplication {
 
 	public static void main(String[] args) {
