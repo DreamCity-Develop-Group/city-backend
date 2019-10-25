c38b6a16d21f65f5dbc8fb5ff41b762a143dcb36
liunh1220 <547119066@qq.com>
2019-10-25 14:16:27 +0800

    转账提现自动审核

--- src/main/java/com/dream/city/job/WithdrawVerifyJob.java
+++ src/main/java/com/dream/city/job/WithdrawVerifyJob.java
@@ -3,13 +3,14 @@ package com.dream.city.job;
 import com.alibaba.fastjson.JSON;
 import com.dream.city.base.Result;
 import com.dream.city.base.model.Page;
-import com.dream.city.base.model.enu.InvestStatus;
+import com.dream.city.base.model.entity.PlayerTrade;
+import com.dream.city.base.model.enu.TradeStatus;
+import com.dream.city.base.model.enu.TradeType;
 import com.dream.city.base.model.enu.VerifyStatus;
-import com.dream.city.base.model.req.InvestOrderReq;
 import com.dream.city.base.model.req.VerifyReq;
-import com.dream.city.base.model.resp.InvestOrderResp;
-import com.dream.city.service.invest.OrderService;
-import com.dream.city.service.verify.InvestVerifyHandleService;
+import com.dream.city.base.model.resp.PlayerTradeResp;
+import com.dream.city.service.trade.PlayerTradeService;
+import com.dream.city.service.verify.WithdrawVerifyHandleService;
 import com.github.pagehelper.PageInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.JobExecutionContext;
@@ -21,18 +22,18 @@ import org.springframework.util.CollectionUtils;
 import java.util.List;
 
 /**
- * 投资预约自动审核
+ * 提现自动审核
  */
 @Slf4j
 @Component
-public class InverstVerifyJob extends QuartzJobBean {
+public class WithdrawVerifyJob extends QuartzJobBean {
 
 
     @Autowired
-    private InvestVerifyHandleService investVerifyHandleService;
+    private WithdrawVerifyHandleService verifyHandleService;
     @Autowired
-    private OrderService orderService;
-    private String jobType = "投资预约";
+    private PlayerTradeService tradeService;
+    private String jobType = "提现";
 
 
     @Override
@@ -50,14 +51,15 @@ public class InverstVerifyJob extends QuartzJobBean {
     private void executeTask() {
         log.info(">>>>>>>>>>> 处理"+jobType+"自动审核业务...");
 
-        InvestOrderReq getDataReq = new InvestOrderReq();
-        getDataReq.setOrderState(InvestStatus.SUBSCRIBED.name());
+        PlayerTrade getDataReq = new PlayerTrade();
+        getDataReq.setTradeType(TradeType.WITHDRAW.getCode());
+        getDataReq.setTradeStatus(TradeStatus.FREEZE.getCode());
 
         Page pageReq = new Page(0,10000000);
         pageReq.setCondition(getDataReq);
 
-        PageInfo<InvestOrderResp> respPageInfo =  orderService.getInvestOrderList(pageReq);
-        List<InvestOrderResp> dataList = null;
+        PageInfo<PlayerTradeResp> respPageInfo =  tradeService.getPlayerTradeList(pageReq);
+        List<PlayerTradeResp> dataList = null;
         if (respPageInfo != null){
             dataList = respPageInfo.getList();
         }
@@ -67,12 +69,12 @@ public class InverstVerifyJob extends QuartzJobBean {
 
         VerifyReq verifyReq = null;
         Result result = null;
-        for (InvestOrderResp resp : dataList){
+        for (PlayerTradeResp resp : dataList){
             verifyReq = new VerifyReq();
-            verifyReq.setOrderId(resp.getOrderId());
+            verifyReq.setTradeId(resp.getTradeId());
             verifyReq.setVerifyStatus(VerifyStatus.PASS.getCode());
             verifyReq.setVerifyDesc(jobType+"自动审核");
-            result = investVerifyHandleService.subscribeOrderVerify(verifyReq);
+            result = verifyHandleService.withdrawVerify(verifyReq);
             log.info(jobType+"自动审核结果:"+JSON.toJSONString(result));
         }
 
